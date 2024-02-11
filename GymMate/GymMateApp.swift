b90706f97583a5f5934e3cae3ddb990b3c7019cb
luismanuelramirezvargas <luis.manuel.ramirez.91@gmail.com>
2024-02-10 18:26:18 -0600

    Initial Commit

---
+++ GymMate/GymMateApp.swift
@@ -0,0 +1,17 @@
+//
+//  GymMateApp.swift
+//  GymMate
+//
+//  Created by Luis Manuel Ramirez Vargas on 10/02/24.
+//
+
+import SwiftUI
+
+@main
+struct GymMateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
